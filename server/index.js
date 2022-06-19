a4c8e545ba398fee93b54c52bce10b29b3dc7771
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-06-19 11:52:49 -0230

    Created userModel for messages

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+const CONNECTION_URL  ='mongodb+srv://adi:sahara123@cluster0.drtgg.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT ||5000
 const server = express()
 
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+console.log('Database connected, PORT',PORT);
+}).catch((err)=>{
 
+})
 server.get('/',(req,res)=>{
     res.send('Server ');
 })
-server.listen(3000,()=>{
+server.listen(5000,()=>{
     console.log('I dont wanna do this')
 })
