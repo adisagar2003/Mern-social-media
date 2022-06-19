96a379a045cf03fdfa0feb5776908018c8e892e5
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-06-19 10:49:15 -0230

    Added google O auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Navbar from './Components/Navbar'
+import SignIn from './Components/Auth/SignIn'
+import { GoogleLogin } from '@react-oauth/google';
+import {GoogleOAuthProvider} from '@react-oauth/google';
+
 
 function App() {
   const [count, setCount] = useState(0)
+const responseGoogle =async (res)=>{
+ console.log(res)
+}
 
+//236836718639-hol81mpdksfikn4354praeabvvst4tp4.apps.googleusercontent.com
+//"You have created a new client application that uses libraries for user authentication or authorization that will soon be deprecated. New clients must use the new libraries instead; existing clients must also migrate before these libraries are deprecated. See the [Migration Guide](https://developers.google.com/identity/gsi/web/guides/gis-migration) for more information."{error: 'idpiframe_initialization_failed', details: 'You have created a new client application that use…i/web/guides/gis-migration) for more information.'}
   return (
     <div className="App">
-    <Navbar />
+   
+<GoogleOAuthProvider clientId="236836718639-hol81mpdksfikn4354praeabvvst4tp4.apps.googleusercontent.com">
+<GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>;
+  </GoogleOAuthProvider>;
+    
+<SignIn />
     </div>
   )
 }
